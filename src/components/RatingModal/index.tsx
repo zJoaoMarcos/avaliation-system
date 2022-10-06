5cd9dbf9307c56bdfc3b871b362815c34a01491a
João Matos <joaomarcosm.silva@outlook.com>
2022-10-06 17:59:47 -0300

    working in design of figma

--- src/components/RatingModal/index.tsx
+++ src/components/RatingModal/index.tsx
@@ -4,6 +4,7 @@ import Rating from "@mui/material/Rating";
 import { useForm, Controller } from "react-hook-form";
 import api from "../../services/api";
 import { useAuth } from "../../contexts/authProvider";
+import { Star } from "phosphor-react";
 
 type EmployeeAvaliation = Omit<Employees, "ratings">;
 
@@ -58,83 +59,193 @@ export function RatingModal({ name, email, department }: EmployeeAvaliation) {
   return (
     <Dialog.DialogPortal>
       <Dialog.DialogOverlay className="bg-black/60 inset-0 fixed" />
-      <Dialog.Content className="w-[600px] h-[95%] flex flex-col justify-between bg-[#CCCCCC] py-6 px-8 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg shadow-black/25">
-        <Dialog.DialogTitle className="w-full flex flex-row items-center justify-center text-xl font-bold">
+      <Dialog.Content className="w-[700px] h-[95%] flex flex-col justify-between bg-white py-6 px-8 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg shadow-black/25">
+        <Dialog.DialogTitle className="w-full flex flex-row items-center justify-center text-2xl font-bold">
           Avaliação
         </Dialog.DialogTitle>
-        <Dialog.DialogDescription className="font-semibold">
+        <Dialog.DialogDescription className="flex flex-col font-semibold">
           {name}
-          <span>{department}</span>
+          <span className="font-normal">{department}</span>
         </Dialog.DialogDescription>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="h-5/6 flex flex-col justify-between"
+          className="h-full flex flex-col justify-between overflow-y-auto"
         >
-          <label>Nota1:</label>
-          <Controller
-            name="note1"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota2:</label>
-          <Controller
-            name="note2"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota3:</label>
-          <Controller
-            name="note3"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota4:</label>
-          <Controller
-            name="note4"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota5:</label>
-          <Controller
-            name="note5"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota6:</label>
-          <Controller
-            name="note6"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota7:</label>
-          <Controller
-            name="note7"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota8:</label>
-          <Controller
-            name="note8"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-          <label>Nota9:</label>
-          <Controller
-            name="note9"
-            control={control}
-            defaultValue={1}
-            render={({ field }) => <Rating {...field} name="note1" />}
-          />
-
-          <button type="submit">Enviar</button>
+          <div className="flex flex-col space-y-1 overflow-y-auto py-1">
+            <div className="flex flex-col p-1 rounded-lg shadow-md shadow-black/20">
+              <label>Nota1:</label>
+              <Controller
+                name="note1"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota2:</label>
+              <Controller
+                name="note2"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota3:</label>
+              <Controller
+                name="note3"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota4:</label>
+              <Controller
+                name="note4"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota5:</label>
+              <Controller
+                name="note5"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota6:</label>
+              <Controller
+                name="note6"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota7:</label>
+              <Controller
+                name="note7"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota8:</label>
+              <Controller
+                name="note8"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col p-2 rounded-lg shadow-md shadow-black/20">
+              <label>Nota9:</label>
+              <Controller
+                name="note9"
+                control={control}
+                defaultValue={0}
+                render={({ field }) => (
+                  <Rating
+                    {...field}
+                    name="note1"
+                    emptyIcon={
+                      <Star size={26} weight="light" color="#000000" />
+                    }
+                    icon={<Star size={26} weight="fill" color="#ffd171" />}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <footer className="flex flex-row justify-end space-x-1">
+            <Dialog.DialogClose className="w-30 py-1.5 px-6 text-[#292194] hover:bg-[#292194] hover:text-white font-semibold border-[#292194] border-2 bg-white rounded-full m-4 shadow-black/25 shadow-md">
+              Cancelar
+            </Dialog.DialogClose>
+            <button
+              type="submit"
+              className="w-30 py-1.5 px-6 text-white border-2 border-transparent hover:bg-white hover:text-[#292194] hover:border-[#292194] hover:border-2 font-semibold bg-[#292194] rounded-full m-4 shadow-black/25 shadow-md"
+            >
+              Enviar
+            </button>
+          </footer>
         </form>
       </Dialog.Content>
     </Dialog.DialogPortal>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { MagnifyingGlass } from "phosphor-react";
-import { useEffect, useState } from "react";
+import { MagnifyingGlass, Star } from "phosphor-react";
 import { RatingModal } from "../../components/RatingModal";
 import { useFetch } from "../../hooks/useFetch";
 import { useAuth } from "../../contexts/authProvider";
@@ -9,69 +8,77 @@ export interface Employees {
   name: string;
   email: string;
   department: string;
-  ratings: string[] | number[];
+  ratings: Array<Ratings>;
+}
+
+export interface Ratings {
+  whoVote: string;
 }
 
 export function Home() {
   const { user } = useAuth();
-
-  console.log(user);
 
   const { data: repositories } = useFetch<Employees[]>(
     `${import.meta.env.VITE_API_AVALIATION_SYSTEM_URL}employees/${user}`
   );
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center px-72 bg-zinc-500">
+    <div className="w-screen h-screen flex flex-col justify-center items-center px-72 bg-[#DBDBDB]">
       <header className="w-full flex flex-row mt-14 mb-5">
-        <h2 className="pl-6 text-5xl font-black text-white text-start">
+        <h2 className="pl-6 text-5xl font-bold text-black text-start">
           Funcionários
         </h2>
       </header>
-      <div className="w-full flex flex-col py-5 px-4 items-center gap-2 bg-gradient-to-b from-zinc-400 via-zinc-400  to-zinc-600 rounded-lg">
-        <div className="w-full flex flex-row py-2 px-4 space-x-2 rounded-lg bg-zinc-200">
+
+      <div className="w-full flex flex-col py-6 px-4 items-center gap-3 ">
+        <div className="w-full flex flex-row py-2 px-4 space-x-2 rounded-lg bg-white shadow-md shadow-black/25 ">
           <a href="">
             <MagnifyingGlass size={30} />
           </a>
           <input
             type="text"
             placeholder="Pesquisar"
-            className="text-xl font-medium focus:outline-none placeholder:font-bold placeholder:text-black placeholder:focus:opacity-25 bg-zinc-200"
+            className="text-xl font-normal focus:outline-none placeholder:font-normal placeholder:text-black placeholder:focus:opacity-25"
           />
         </div>
 
-        <table className="w-full bg-zinc-200 rounded-lg">
-          <thead className="">
-            <tr>
-              <th className="p-3 text-lg font-semibold tracking-wide">Name</th>
-              <th className="p-3 text-lg font-semibold tracking-wide">
-                Departameto
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {repositories?.map((employee) => (
-              <tr key={employee.email}>
-                <td className="p-3 text-center text-base">{employee.name}</td>
-                <td className="p-3 text-center text-base">
-                  {employee.department}
-                </td>
-                <td>
-                  <Dialog.Root>
-                    <Dialog.DialogTrigger className="w-36 py-1 px-5 bg-zinc-600 hover:bg-zinc-700 font-black text-white rounded-md gap-2 flex flex-row items-center justify-start">
-                      Avaliar
-                    </Dialog.DialogTrigger>
-                    <RatingModal
-                      department={employee.department}
-                      name={employee.name}
-                      email={employee.email}
-                    />
-                  </Dialog.Root>
-                </td>
+        <div className="w-full py-4 bg-white rounded-lg shadow-md shadow-black/25">
+          <table className="w-full ">
+            <thead className="border-b-2 border-separate ">
+              <tr>
+                <th className="p-3 text-lg font-bold tracking-wide">Name</th>
+                <th className="p-3 text-lg font-bold tracking-wide">
+                  Departameto
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {repositories?.map((employee) => (
+                <tr key={employee.email}>
+                  <td className="font-semibold text-center text-base p-6">
+                    {employee.name}
+                  </td>
+                  <td className="font-semibold text-center text-base p-6">
+                    {employee.department}
+                  </td>
+                  <td>
+                    <Dialog.Root>
+                      <Dialog.DialogTrigger className="w-36 py-1.5 px-6 bg-[#4F46BB] hover:bg-[#292194] font-medium text-white rounded-full gap-2 flex flex-row items-center justify-start m-4 shadow-black/25 shadow-md">
+                        <Star size={26} color="#ffd171" weight="bold" />
+                        Avaliar
+                      </Dialog.DialogTrigger>
+                      <RatingModal
+                        department={employee.department}
+                        name={employee.name}
+                        email={employee.email}
+                      />
+                    </Dialog.Root>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
