c3deaf602816ee97a1009bc8d7a6c1cb6c083f0d
João Matos <joaomarcosm.silva@outlook.com>
2022-12-07 11:11:28 -0300

    test auth vercel 2.5

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,15 @@
-import { useIsAuthenticated } from "@azure/msal-react";
+import { InteractionStatus } from "@azure/msal-browser";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   const isAuthenticated = useIsAuthenticated();
-  return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
+  const { inProgress } = useMsal();
+
+  return isAuthenticated && inProgress === InteractionStatus.None ? (
+    <AppRoutes />
+  ) : (
+    <AuthRoutes />
+  );
 }
