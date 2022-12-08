d33d12a6d3a61ad4d778564254d24d5184ac2176
João Matos <joaomarcosm.silva@outlook.com>
2022-12-08 16:21:23 -0300

    working in loading 3.3

---
+++ src/components/AnimationLoading/index.tsx
@@ -0,0 +1,16 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import animation from "../../assets/loading-stars.json";
+
+export function AnimationLoading() {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <Player
+        src={animation}
+        style={{ width: "280px" }}
+        autoplay
+        loop
+        speed={1}
+      ></Player>
+    </div>
+  );
+}
--- src/pages/Avaliation/index.tsx
+++ src/pages/Avaliation/index.tsx
@@ -1,10 +1,11 @@
+import { AnimationLoading } from "../../components/AnimationLoading";
 import { Header } from "../../components/Header/index";
 import { SearchButton } from "../../components/Search";
 import { TableRowEmployee } from "../../components/TableRowEmployee";
 import { useRating } from "../../hooks/useRating";
 
 export function Avaliation() {
-  const { employees, search, filteredEmployees } = useRating();
+  const { employees, search, filteredEmployees, isLoading } = useRating();
 
   return (
     <div className="w-full min-h-screen overflow-x-hidden flex flex-col items-center bg-[#DBDBDB]">
@@ -13,15 +14,15 @@ export function Avaliation() {
       <div className="w-screen md:w-3/4 flex flex-col py-6 px-4 items-center gap-3 ">
         <SearchButton />
         <div className="w-full py-4 bg-white rounded-lg shadow-md shadow-black/25">
+          {isLoading && <AnimationLoading />}
           <table className="w-full">
-            <thead className="border-b-2 border-separate hidden ">
+            {/* <thead className="border-b-2 border-separate hidden ">
               <tr>
-                <th></th>
-                <th className="p-3 text-left text-sm">Nome</th>
-                <th className="p-3 hidden sm:block text-sm">Departamento</th>
+              <th></th>
+              <th className="p-3 text-left text-sm">Nome</th>
+              <th className="p-3 hidden sm:block text-sm">Departamento</th>
               </tr>
-            </thead>
-
+            </thead> */}
             <tbody className="">
               {search.length > 0 ? (
                 <>
