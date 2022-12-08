c3dadfdec15499f6dd0961c6e2be56a7bb10de75
João Matos <joaomarcosm.silva@outlook.com>
2022-12-08 10:01:07 -0300

    wroking in responsive 3.0

--- src/components/AnimationSignIn/index.tsx
+++ src/components/AnimationSignIn/index.tsx
@@ -1,12 +1,16 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import animation from "../../assets/animation.json";
 
+interface AnimationSignInProps {
+  width: string;
+}
+
 export function AnimationSignIn() {
   return (
-    <div className="w-full">
+    <div className="w-full flex items-center justify-center">
       <Player
         src={animation}
-        style={{ width: "350px" }}
+        style={{ width: "280px" }}
         autoplay
         loop
         speed={1}
